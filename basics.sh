bdc9c5ef237b6e3f54f732d3157c2709b4200771
sanju <sanjusk08gmail.com>
2022-07-18 11:38:06 +0530

    added hello world to basics.sh

---
+++ basics.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+
+echo "hello world" 
+
+name="sanju"
+echo "name: $name"
+
+echo "0: $0"
+echo "1: $1"
+echo "empty string"
+echo "*: $*"
+echo "@: $@"
+echo "number of args: #: $#"
+echo "pid: $$"
+
+echo "ending"
